efb63a66887228e13594e2e885aee3dd361c8b42
GayatreeUser <162143894+GayatreeUser@users.noreply.github.com>
2024-03-13 18:57:37 +0530

    main.tf

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_instance" "testinst" {
-  ami           = "ami-0c94855ba95c574c8" # replace with your desired AMI ID
-  instance_type = "t2.micro" # replace with your desired instance type
+  ami           = "ami-0c94855ba95c574c8" #  AMI ID
+  instance_type = "t2.micro" 
 
   tags = {
     Name = "example-instance"
