e2e633215013508895efef5637e106b097e5c17f
GayatreeUser <patragayatree.1995@gmail.com>
2024-05-02 17:00:33 +0530

    inst name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "test_instance" {
+resource "aws_instance" "test_instance0" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   tags = {
