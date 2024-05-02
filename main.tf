a8407271bf4e1529b2c9dba40e358862634e4ea4
GayatreeUser <patragayatree.1995@gmail.com>
2024-05-02 17:44:18 +0530

    inst name change

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "test_instance0" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   tags = {
-    Name = "test_instance1"
+    Name = "test_instance2"
   }
 }
